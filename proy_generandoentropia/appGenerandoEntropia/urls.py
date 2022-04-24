69f6526af14961e9fb9cdc5304e160dac4eed15f
Dario Sosa <dariogss@gmail.com>
2022-04-24 10:59:24 -0300

    agrego pagina para loguearse y un rudimentario formulario

--- proy_generandoentropia/appGenerandoEntropia/urls.py
+++ proy_generandoentropia/appGenerandoEntropia/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("miscosas", views.miscosas, name="miscosas"),
     path("loquenosepuededecir", views.loquenosepuededecir, name="loquenosepuededecir"),
     path("aikalaperra", views.aikalaperra, name="aikalaperra"),
+    path("loquenosepuededecirLogueado", views.loquenosepuededecirLogueado, name="loquenosepuededecirLogueado"),
+    #path("logout",views.logout_view, name="logout"),
 ]
--- proy_generandoentropia/appGenerandoEntropia/views.py
+++ proy_generandoentropia/appGenerandoEntropia/views.py
@@ -1,5 +1,9 @@
 import re
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 def index(request):
@@ -12,7 +16,34 @@ def miscosas(request):
     return render(request, "appGenerandoEntropia/miscosas.html")
 
 def loquenosepuededecir(request):
-    return render(request, "appGenerandoEntropia/loquenosepuededecir.html")
+    #return render(request, "appGenerandoEntropia/loquenosepuededecir.html")
+    if request.method == "POST":
+        #Lee lo que se haya escrito en los campos del form
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        #Verifica si usuario y contraseña son correctos (los ingresados en el form)
+        user = authenticate(request, username=username, password=password)
+
+        #Si se devuelve un objeto USER, se puede loguear y se rutea a la pagina loquenosepuededecirLogueado
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("loquenosepuededecirLogueado"))
+        #Si el usuario y/o contraseña no son correctos, nos redirige a la página para loguearnos, con un mensaje del error
+        else:
+            return render(request, "appGenerandoEntropia/loquenosepuededecir.htom", {
+                "message": "usuario y/o contraseña inválido"
+            })
+
+    return render(request, "appGenerandoEntropia/loquenosepuededecir.html")    
+
+def loquenosepuededecirLogueado(request):
+    #return render(request, "appGenerandoEntropia/loquenosepuededecirLogueado.html")
+    # If no user is signed in, return to login page:     
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("loquenosepuededecir"))
+    return render(request, "appGenerandoEntropia/loquenosepuededecirLogueado.html")
 
 def aikalaperra(request):
     return render(request, "appGenerandoEntropia/aikalaperra.html")
+
